a0327dba04e60c3b8623eeefabc03083c9d346ea
James McDonald <james@jamesmcdonald.com>
2020-12-01 12:34:13 +0100

    Add nice lazy day1 solution in Go

---
+++ day1/2020.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func readinput(filename string) []int {
+	result := []int{}
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, val)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
+
+func main() {
+	values := readinput("input")
+
+	// Super lazy nested loops win!
+	outerdone := false
+	innerdone := false
+	for i := range values {
+		for j := range values {
+			if i == j {
+				continue
+			}
+			if values[i]+values[j] == 2020 {
+				fmt.Println(values[i] * values[j])
+				innerdone = true
+				break
+			}
+			for k := range values {
+				if k == j || k == i {
+					continue
+				}
+				if values[i]+values[j]+values[k] == 2020 {
+					fmt.Println(values[i] * values[j] * values[k])
+					outerdone = true
+					break
+				}
+			}
+		}
+		if innerdone && outerdone {
+			break
+		}
+	}
+}
